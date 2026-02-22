dbe9cefdd0d7b88dae18a2ed804b1085fb7b68c4
Vhora Tanvirahmad Yakubbhai <136716771+Tanvir1610@users.noreply.github.com>
2026-02-23 05:29:16 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,10 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
-}, {
-  authorizedParties: ["https://tarschat.vercel.app"],
-});
+export function middleware(req: NextRequest) {
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: [],
 };
