f5fff768aa7262a9106f1447fcd4963f9780b7da
Vhora Tanvirahmad Yakubbhai <136716771+Tanvir1610@users.noreply.github.com>
2026-02-22 15:01:57 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
@@ -8,17 +7,13 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    const { userId } = await auth();
-    if (!userId) {
-      const signInUrl = new URL("/sign-in", req.url);
-      return NextResponse.redirect(signInUrl);
-    }
+    await auth.protect();
   }
 });
 
 export const config = {
   matcher: [
-    "/((?!_next|favicon.ico|.*\\..*).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
   ],
 };
