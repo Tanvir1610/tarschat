bd1937551d81a70f0feac40f51dd71d9ffb0ad42
Vhora Tanvirahmad Yakubbhai <136716771+Tanvir1610@users.noreply.github.com>
2026-02-24 07:10:14 +0530

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,22 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export function middleware(req: NextRequest) {
-  return NextResponse.next();
-}
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+    authorizedParties: ['https://tarschat.vercel.app/'];
+  }
+  
+});
 
 export const config = {
-  matcher: [],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
